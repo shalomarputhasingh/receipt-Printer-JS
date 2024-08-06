d9164d34ec329b39f5abea3f34346baa4a93ac50
shalomarputhasingh <shalomdevofficial@gmail.com>
2024-08-06 11:28:20 +0530

    Complated Version of the Project

--- printingReceipt/printer/models.py
+++ printingReceipt/printer/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+#Basic Student Model
+
+#In the admin panel you can add new Students
+class Students (models.Model):
+    unique_id = models.IntegerField(primary_key=True)
+    name = models.CharField(null=False, max_length=200)
+    def __int__(self):
+        return self.unique_id
+#Basic Bill Structure
+class Bills(models.Model):
+    student_id = models.ForeignKey(Students,on_delete=models.CASCADE)
+    amount = models.IntegerField(null=False)
+    description = models.TextField(null=False)
+    date = models.DateField(null=False)
--- printingReceipt/printer/views.py
+++ printingReceipt/printer/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Students, Bills
 # Create your views here.
+def studentBill(request):
+    student = Students.objects.get(name = "Kevin") #Got the particular Student Object
+    bills = Bills.objects.get(student_id = student.unique_id)
+    context = {'student':student,'bill':bills}
+    return render(request,'printer/receipt.html',context)
